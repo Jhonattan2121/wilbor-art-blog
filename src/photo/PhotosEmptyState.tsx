e13c0471ff082cc53117ec6ef24f6fd261276a32
jhonattan2121 <devferri22@gmail.com>
2025-02-23 16:50:29 -0300

    feat: implement empty state for photo gallery

--- src/photo/PhotosEmptyState.tsx
+++ src/photo/PhotosEmptyState.tsx
@@ -1,11 +1,7 @@
-import AdminCTA from '@/admin/AdminCTA';
+import { IS_SITE_READY } from '@/app/config';
 import Container from '@/components/Container';
 import SiteGrid from '@/components/SiteGrid';
-import { IS_SITE_READY } from '@/app/config';
-import { PATH_ADMIN_CONFIGURATION } from '@/app/paths';
-import AdminAppConfiguration from '@/admin/AdminAppConfiguration';
 import { clsx } from 'clsx/lite';
-import Link from 'next/link';
 import { HiOutlinePhotograph } from 'react-icons/hi';
 
 export default function PhotosEmptyState() {
@@ -26,27 +22,7 @@ export default function PhotosEmptyState() {
           )}>
             {!IS_SITE_READY ? 'Finish Setup' : 'Setup Complete!'}
           </div>
-          {!IS_SITE_READY
-            ? <AdminAppConfiguration simplifiedView />
-            : <div className="max-w-md text-center space-y-6">
-              <div className="space-y-2">
-                <div>
-                  Add your first photo:
-                </div>
-                <AdminCTA />
-              </div>
-              <div>
-                Change the name of this blog and other configuration
-                by editing environment variables referenced in
-                {' '}
-                <Link
-                  href={PATH_ADMIN_CONFIGURATION}
-                  className="text-main hover:underline"
-                >
-                  /admin/configuration
-                </Link>
-              </div>
-            </div>}
+         
         </Container>}
     />
   );
