742da270075c02e8ec6f3630a1a209aca5b527df
jhonattan2121 <devferri22@gmail.com>
2025-03-12 17:06:37 -0300

    feat: fixing tags

--- src/photo/PhotoGridPage.tsx
+++ src/photo/PhotoGridPage.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { Cameras } from '@/camera';
+import CollapsibleFooterTags from '@/components/CollapsibleFooterTags';
 import { FilmSimulations } from '@/simulation';
 import { useAppState } from '@/state/AppState';
 import { Tags } from '@/tag';
 import clsx from 'clsx';
 import { useEffect } from 'react';
-import PhotoGridSidebar from './PhotoGridSidebar';
 import PhotoGridContainer, { Photo } from './PhotoProjectsContainer';
+
 const PATH_GRID_INFERRED = 'projects';
 
 export default function PhotoGridPage({
@@ -24,6 +25,10 @@ export default function PhotoGridPage({
 }) {
   const { setSelectedPhotoIds } = useAppState();
 
+  useEffect(() => {
+    console.log('Tags recebidas:', tags);
+  }, [tags]);
+
   useEffect(
     () => () => setSelectedPhotoIds?.(undefined),
     [setSelectedPhotoIds],
@@ -40,42 +45,51 @@ export default function PhotoGridPage({
       )}
     />;
   return (
-    <PhotoGridContainer
-      cacheKey={`page-${PATH_GRID_INFERRED}`}
-      media={photos.map(photo => ({
-        ...photo,
-        type: photo.type === 'video' ? 'video' : 'photo',
-        thumbnailSrc: photo.type === 'video' ? photo.thumbnailSrc : undefined,
-        videoUrl: photo.type === 'video' ? photo.src : undefined
-      }))}
-      sidebar={
-        <div className={clsx(
-          'sticky top-0 pt-5',
-          'w-full max-w-[250px]',
-          'md:min-w-[200px]'
-        )}>
-          {renderGuard('top')}
-          <div className={clsx(
-            'overflow-y-auto',
-            'max-h-[calc(100vh-100px)]',
-            'scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-700',
-            'scrollbar-track-transparent',
-            'px-2 py-4'
-          )}>
-            <PhotoGridSidebar {...{
-              tags,
-              cameras,
-              simulations,
-            }}
-            />
-            <div className="py-4 text-sm  dark:text-gray-400">
-              {photosCount} {photosCount === 1 ? 'photo' : 'photos'}
+    <>
+      <div className="pb-20">
+        <PhotoGridContainer
+          cacheKey={`page-${PATH_GRID_INFERRED}`}
+          media={photos.map(photo => ({
+            ...photo,
+            type: photo.type === 'video' ? 'video' : 'photo',
+            thumbnailSrc: photo.type === 'video' ? photo.thumbnailSrc : undefined,
+            videoUrl: photo.type === 'video' ? photo.src : undefined
+          }))}
+          sidebar={
+            <div className={clsx(
+              'sticky top-0 pt-5',
+              'w-full max-w-[250px]',
+              'md:min-w-[200px]'
+            )}>
+              {renderGuard('top')}
+              <div className={clsx(
+                'overflow-y-auto',
+                'max-h-[calc(100vh-100px)]',
+                'scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-700',
+                'scrollbar-track-transparent',
+                'px-2 py-4'
+              )}>
+
+
+                <div className="py-4 text-sm  dark:text-gray-400">
+                  {photosCount} {photosCount === 1 ? 'photo' : 'photos'}
+                </div>
+              </div>
+              {renderGuard('bottom')}
             </div>
-          </div>
-          {renderGuard('bottom')}
-        </div>
-      }
-      canSelect
-    />
+
+          }
+          canSelect
+        />
+      </div>
+      {Object.keys(tags).length > 0 ? (
+        <CollapsibleFooterTags tags={tags.map(tag => tag.tag)} />
+
+      ) : (
+
+        console.log('Tags não disponíveis')
+      )}
+
+    </>
   );
 }
